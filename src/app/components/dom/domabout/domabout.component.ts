daf8b1e20010ec4444e69399e6cc270952a637d5
Jos Vermoesen <info@rv.be>
2020-03-05 12:40:15 +0100

    Angular 9 start

---
+++ src/app/components/dom/domabout/domabout.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-domabout',
+  templateUrl: './domabout.component.html',
+  styleUrls: ['./domabout.component.css']
+})
+export class DomAboutComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+}
